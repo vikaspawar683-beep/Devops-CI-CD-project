a990eba58a1a5fc6daaf70245744dd6814501346
vikaspawar683-beep <vikaspawar683@gmail.com>
2026-03-11 23:19:11 +0530

    Add AWS Application Load Balancer configuration

---
+++ load-balancer.tf
@@ -0,0 +1,15 @@
+resource "aws_lb" "app_lb" {
+  name               = "devops-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+
+  subnets = [
+    aws_subnet.public_subnet1.id,
+    aws_subnet.public_subnet2.id
+  ]
+
+  tags = {
+    Name = "DevOps-ALB"
+  }
+}
