2058d9adceffec65d29bd1d4a187818ae9ca0334
vikaspawar683-beep <vikaspawar683@gmail.com>
2026-03-14 23:03:42 +0530

    Update ec2.tf

--- tarraform/ec2.tf
+++ tarraform/ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "app1" {
   ami                    = "ami-0f5ee92e2d63afc18"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_subnet1.id
+  key_name               = "final-devops-key"
   vpc_security_group_ids = [aws_security_group.devops_sg.id]
 
   tags = {
@@ -13,6 +14,7 @@ resource "aws_instance" "app2" {
   ami                    = "ami-0f5ee92e2d63afc18"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_subnet2.id
+  key_name               = "final-devops-key"
   vpc_security_group_ids = [aws_security_group.devops_sg.id]
 
   tags = {
