c01ae1b148d2d3a6f497f9aab0c6a1fdb221a8e1
vikaspawar683-beep <vikaspawar683@gmail.com>
2026-03-16 17:36:55 +0530

    Update security-group.tf

--- tarraform/security-group.tf
+++ tarraform/security-group.tf
@@ -35,3 +35,27 @@ resource "aws_security_group" "devops_sg" {
     Name = "devops-sg"
   }
 }
+
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Allow HTTP"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
