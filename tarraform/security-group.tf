e4f5d44ead9e65d3fccfa0812efd0f4f4388ab02
vikaspawar683-beep <vikaspawar683@gmail.com>
2026-03-16 17:31:59 +0530

    Update security-group.tf

--- tarraform/security-group.tf
+++ tarraform/security-group.tf
@@ -1,17 +1,11 @@
 resource "aws_security_group" "devops_sg" {
-  name   = "devops-sg"
-  vpc_id = aws_vpc.main.id
+  name        = "devops-sg"
+  description = "Allow SSH, HTTP, Jenkins"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port   = 22
     to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -22,16 +16,22 @@ resource "aws_security_group" "devops_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-resource "aws_security_group" "alb_sg" {
-  name   = "alb-sg"
-  vpc_id = aws_vpc.main.id
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "devops-sg"
   }
 }
