879723270842bbc64433e7d6a67d8099bed28376
vikaspawar683-beep <vikaspawar683@gmail.com>
2026-03-16 19:18:02 +0530

    Update target-group.tf

--- tarraform/target-group.tf
+++ tarraform/target-group.tf
@@ -1,8 +1,8 @@
 resource "aws_lb_target_group" "tg" {
-  name     = "devops-tg"
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+  name_prefix = "dev-"
+  port        = 8080
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
 
   health_check {
     path                = "/"
@@ -13,6 +13,10 @@ resource "aws_lb_target_group" "tg" {
     timeout             = 5
     healthy_threshold   = 2
     unhealthy_threshold = 2
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 
   tags = {
